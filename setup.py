e9a831889e69392d27da0e8c34b7fcc739af76d3
Wilson Júnior <wilsonpjunior@gmail.com>
2015-11-10 10:15:34 -0200

    Require new setuptools
    Signed-off-by: Wilson Júnior <wilsonpjunior@gmail.com>

--- setup.py
+++ setup.py
@@ -28,6 +28,6 @@ setup(name="tornado-measures",
       ),
       include_package_data=True,
       zip_safe=True,
-      install_requires=['tornado', 'measures'],
+      install_requires=['setuptools>=17.1', 'tornado', 'measures'],
       tests_require=["nose==1.2.1", "pep8==1.4.1", "mock==1.0.1"],
       )
